1e3e23fa2f19ca263575f9efb8179a0c6296fb3d
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2024-03-09 15:22:25 +0530

    added some photos

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", paymentRoute);
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://vegan-hub.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'http://vegan-hub.vercel.app');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });

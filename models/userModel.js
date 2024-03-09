ca3e37931da12e1c324b89b819e68d23feba2ba6
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2024-03-09 14:36:24 +0530

    added some photos

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    age: Number,
+    gender: String,
+    delivery: String,
 });
 
 export const User = mongoose.model("User", userSchema);
